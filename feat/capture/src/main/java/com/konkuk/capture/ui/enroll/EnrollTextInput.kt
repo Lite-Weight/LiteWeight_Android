8e1854396f3b139b97208da540732b7f41adb296
chanu <cw8881@naver.com>
2023-06-04 02:33:39 +0900

    feat : 등록방법 선택에 따른 다음화면 넘어가기 구현

---
+++ feat/capture/src/main/java/com/konkuk/capture/ui/enroll/EnrollTextInput.kt
@@ -0,0 +1,12 @@
+package com.konkuk.capture.ui.enroll
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.konkuk.capture.R
+
+class EnrollTextInput : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_enroll_text_input)
+    }
+}
