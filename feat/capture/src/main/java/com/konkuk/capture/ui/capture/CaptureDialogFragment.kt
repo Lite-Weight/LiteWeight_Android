c03c86ab26ea4448f4c08f63fcb10a9d80d2b7d2
yonghanJu <ju990828@naver.com>
2023-06-23 19:04:01 +0900

    feat: 사진 가져오기 옵션 팝업 실패시 동작 추가

--- feat/capture/src/main/java/com/konkuk/capture/ui/capture/CaptureDialogFragment.kt
+++ feat/capture/src/main/java/com/konkuk/capture/ui/capture/CaptureDialogFragment.kt
@@ -1,17 +1,21 @@
 package com.konkuk.capture.ui.capture
 
+import android.annotation.SuppressLint
 import android.app.AlertDialog
 import android.app.Dialog
+import android.content.DialogInterface
 import android.os.Bundle
 import androidx.fragment.app.DialogFragment
 
 class CaptureDialogFragment(
     private val onTakePicture: () -> Unit,
     private val onGetPicture: () -> Unit,
+    private val onDisMiss: () -> Unit,
 ) : DialogFragment() {
 
     private val captureOptions = arrayOf("직접 찍기", "가져오기")
 
+    @SuppressLint("DialogFragmentCallbacksDetector")
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         return activity?.let {
             // Use the Builder class for convenient dialog construction
@@ -27,5 +31,10 @@ class CaptureDialogFragment(
                 }
             builder.create()
         } ?: throw IllegalStateException("Activity cannot be null")
+    }
+
+    override fun onDismiss(dialog: DialogInterface) {
+        onDisMiss()
+        super.onDismiss(dialog)
     }
 }
