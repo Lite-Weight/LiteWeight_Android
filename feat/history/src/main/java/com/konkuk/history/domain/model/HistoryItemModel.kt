2210a9751a27612174b497c579f57eb2c3deb72c
yonghanJu <ju990828@naver.com>
2023-06-29 00:16:05 +0900

    feat: HistoryItemModel 아이탬 ID 추가

--- feat/history/src/main/java/com/konkuk/history/domain/model/HistoryItemModel.kt
+++ feat/history/src/main/java/com/konkuk/history/domain/model/HistoryItemModel.kt
@@ -3,11 +3,12 @@ package com.konkuk.history.domain.model
 import com.konkuk.common.data.FoodInfo
 
 data class HistoryItemModel(
+    val id: Long,
     val date: Long,
     val name: String,
     val calories: Int,
 )
 
 fun FoodInfo.toHistoryItemModel(): HistoryItemModel {
-    return HistoryItemModel(date, name, calories.toInt())
+    return HistoryItemModel(id, date, name, calories.toInt())
 }
