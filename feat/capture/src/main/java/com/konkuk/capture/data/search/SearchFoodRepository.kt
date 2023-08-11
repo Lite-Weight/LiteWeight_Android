23139de8ad8fa01ccff48cc49b3b36ecde932388
yonghanJu <ju990828@naver.com>
2023-08-11 16:52:53 +0900

    feat: 음식 검색 페이징 유즈케이스 구현

---
+++ feat/capture/src/main/java/com/konkuk/capture/data/search/SearchFoodRepository.kt
@@ -0,0 +1,22 @@
+package com.konkuk.capture.data.search
+
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
+import com.konkuk.common.data.FoodInfo
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class SearchFoodRepository @Inject constructor(private val searchFoodDatasource: SearchFoodDataSource) {
+    fun getSearchedItems(name: String): Flow<PagingData<FoodInfo>> {
+        return Pager(
+            config = PagingConfig(pageSize = 10, enablePlaceholders = true),
+            pagingSourceFactory = { loadSearchedFoodItemsPage(name) },
+        )
+            .flow
+    }
+
+    private fun loadSearchedFoodItemsPage(name: String): SearchFoodPagingSource {
+        return SearchFoodPagingSource(name, searchFoodDatasource)
+    }
+}
