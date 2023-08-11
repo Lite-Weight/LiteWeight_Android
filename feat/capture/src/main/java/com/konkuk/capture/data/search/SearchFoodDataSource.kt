a209302045f89fad26b0beb6b68127eeb8cf9c10
yonghanJu <ju990828@naver.com>
2023-08-11 18:13:20 +0900

    feat: 검색 api 빈칸 검색 예외 처리

--- feat/capture/src/main/java/com/konkuk/capture/data/search/SearchFoodDataSource.kt
+++ feat/capture/src/main/java/com/konkuk/capture/data/search/SearchFoodDataSource.kt
@@ -5,9 +5,11 @@ import javax.inject.Inject
 
 class SearchFoodDataSource @Inject constructor(private val searchFoodService: SearchFoodService) {
     suspend fun searchFood(name: String, pageNo: Int): Result<List<FoodInfo>> {
+        val searchName = name.trim()
+        if (searchName.isBlank()) return Result.success(emptyList())
         return kotlin.runCatching {
             requireNotNull(
-                searchFoodService.searchFoodService(name, pageNo).body(),
+                searchFoodService.searchFoodService(searchName, pageNo).body(),
             ).items.map { it.toFoodInfo() }
         }
     }
