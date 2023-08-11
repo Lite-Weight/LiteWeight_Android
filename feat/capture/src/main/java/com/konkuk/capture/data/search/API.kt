542f7e9d73075daee2e0d9afba6ce723bae60623
yonghanJu <ju990828@naver.com>
2023-08-11 15:57:31 +0900

    feat: 검색 기능 관련 api 상수 추가

---
+++ feat/capture/src/main/java/com/konkuk/capture/data/search/API.kt
@@ -0,0 +1,9 @@
+package com.konkuk.capture.data.search
+
+object API {
+    const val BASE_URL = "http://apis.data.go.kr/"
+
+    const val CONTENT_TYPE = "application/json"
+
+    const val SEARCH_FOOD_INFO = "1471000/FoodNtrIrdntInfoService1/getFoodNtrItdntList1"
+}
